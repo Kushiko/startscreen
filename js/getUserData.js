32e179b66f3dbca719ae4bb1cc61678cfb90a5ad
ILYFKA <linux.ilyfka@gmail.com>
2024-11-30 18:13:20 +0700

    repair icon

--- js/getUserData.js
+++ js/getUserData.js
@@ -7,14 +7,6 @@ const setupUserData = async () => {
   const userSocialLinks = userData.socialLinks;
 
   USERNAME_ELEMENT.textContent = userNickname;
-  userSocialLinks.map((eachLink) => {
-    let currentLink = eachLink;
-
-    return (LINKS_GRID.innerHTML += `
-        <a href="${currentLink.url}" target="_blank" class="inline-flex items-center justify-center transition-colors ease-linear border-2 border-transparent rounded-lg hover:border-ctp-blue bg-ctp-base aspect-square hover:bg-ctp-surface0/50">
-          <span class="${currentLink.iconClass} text-6xl" alt="Icon ${currentLink.url}"></span>
-        </a>`);
-  });
 };
 
 setupUserData();
